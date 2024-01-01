6d7b043570441e31fcef05bad19abc35a570006c
sinhhungahihi <sinhhungmaths@gmail.com>
2024-01-01 21:57:46 +0700

    update#2

--- RentApp/admin.py
+++ RentApp/admin.py
@@ -3,3 +3,9 @@ from RentApp.models import *
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Accommodation)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Image)
+admin.site.register(Notification)
+
